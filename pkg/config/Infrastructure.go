3e470fe6dce8ad913d587f0e2949a28127fff3f4
pirotyyy <hiroki1423@docomo.ne.jp>
2024-04-08 08:20:16 +0900

    :sparkles: config

---
+++ pkg/config/Infrastructure.go
@@ -0,0 +1,11 @@
+package config
+
+type Infrastructure struct {
+	GoogleCloud GoogleCloud `yaml:"google_cloud"`
+}
+
+type GoogleCloud struct {
+	ProjectID           string `yaml:"project_id"`
+	UseCredentialsFile  bool   `yaml:"use_credentials_file"`
+	CredentialsFilePath string `yaml:"credentials_file_path"`
+}
