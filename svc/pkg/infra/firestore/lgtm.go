1149e421092f379ce6f3135c4c5aa58ab825aac3
pirotyyy <hiroki1423@docomo.ne.jp>
2024-04-09 22:58:41 +0900

    :sparkles: list method

---
+++ svc/pkg/infra/firestore/lgtm.go
@@ -0,0 +1,39 @@
+package firestore
+
+import (
+	"context"
+	"lgtm-gen/svc/pkg/infra/entity"
+
+	"cloud.google.com/go/firestore"
+)
+
+const LGTMCollectionName = "lgtms"
+
+type LGTMTable struct {
+	fsClient *firestore.Client
+}
+
+func NewLGTMTable() *LGTMTable {
+	return &LGTMTable{}
+}
+
+// List get list of lgtm images data
+func (l *LGTMTable) List() ([]*entity.LGTM, error) {
+	ctx := context.Background()
+	// TODO: pagination
+	docs, err := l.fsClient.Collection("lgtms").Documents(ctx).GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	lgtms := make([]*entity.LGTM, len(docs))
+	for i, doc := range docs {
+		var lgtm = entity.LGTM{}
+		if err := doc.DataTo(&lgtm); err != nil {
+			return nil, err
+		}
+		lgtms[i] = &lgtm
+	}
+
+	return lgtms, nil
+}
